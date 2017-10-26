c755f87ab744cb5875edf65bc5e5c7e3836fe5d4
csergey8 <c.sergey@protonmail.com>
2017-10-26 17:18:08 +0300

    Link to NavLink

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
 
 import 'normalize.css/normalize.css';
@@ -27,10 +27,10 @@ const HelpPage = () => (
 const Header = () => (
     <header>
     <h1>Expensify</h1>
-    <Link to="/">Dashboard</Link>
-    <Link to="/create">Create Expense</Link>
-    <Link to="/edit">Edit Expense</Link>
-    <Link to="/help">Help</Link>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
 );
 

cc23267087f2a271b449e8c73b8fbebc327fe2c8
csergey8 <c.sergey@protonmail.com>
2017-10-26 17:04:57 +0300

    add header and header links

--- src/app.js
+++ src/app.js
@@ -24,12 +24,24 @@ const HelpPage = () => (
     <div> This is help page </div>
 );
 
+const Header = () => (
+    <header>
+    <h1>Expensify</h1>
+    <Link to="/">Dashboard</Link>
+    <Link to="/create">Create Expense</Link>
+    <Link to="/edit">Edit Expense</Link>
+    <Link to="/help">Help</Link>
+    </header>
+);
+
 const NotFoundPage = () => (
-    <div> 404 </div><Link to="/">Go Home</Link>
+    <div> 404 <Link to="/">Go Home</Link></div>
 );
 
 const routes = ( 
     <BrowserRouter>
+    <div>
+    <Header/>
     <Switch>
     <Route path="/" component={ExpenseDashboardPage} exact={true}/>
     <Route path="/create" component={AddExpensePage}/>
@@ -37,6 +49,8 @@ const routes = (
     <Route path="/help" component={HelpPage}/>
     <Route component={NotFoundPage}/>
     </Switch>
+    </div> 
+   
 
     </BrowserRouter>
 );

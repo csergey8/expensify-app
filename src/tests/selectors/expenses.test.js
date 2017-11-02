15aa3388c04eece0abb23157fe9a7a52ff1530b8
csergey8 <c.sergey@protonmail.com>
2017-11-02 10:26:26 +0200

    add test to other selector filters

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -12,13 +12,13 @@ const expenses = [{
     description: 'Rent',
     note: '',
     amount: 1425200,
-    createdAt: -1000
+    createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
     id: 3,
     description: 'Credit ccard',
     note: '',
     amount: 5600,
-    createdAt: 2000
+    createdAt: moment(0).add(4, 'days').valueOf()
 }]
 
 test('should filter by text value', () => {
@@ -31,5 +31,57 @@ test('should filter by text value', () => {
     const result = selectExpenses(expenses, filters);
     expect(result).toEqual([
         expenses[2], expenses[1]
+    ]);
+});
+
+test('should filter by start date', () =>{
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: moment(0),
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([
+        expenses[2], expenses[0]
+    ]);
+});
+
+test('should filter by end date', () =>{
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: moment(0)
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([
+        expenses[0], expenses[1]
+    ]);
+});
+
+test('should sort by date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([
+        expenses[2], expenses[0], expenses[1]
+    ]);
+});
+
+test('should sort by amount', () => {
+    const filters = {
+        text: '',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([
+        expenses[1], expenses[2], expenses[0]
     ]);
 });

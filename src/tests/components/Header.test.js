b42f77ec628e3efc6fbe39ef3ab3ace974d6ad9d
csergey8 <c.sergey@protonmail.com>
2017-11-02 14:22:04 +0200

    install enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,2 +1,11 @@
+import React from 'react';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
+import Header from '../../components/Header';
+
+test('should render correctly', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    console.log(renderer.getRenderOutput());
+});
 

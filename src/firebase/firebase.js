9ab922b17fb236104bb1dcefdcf40ea7acf503ed
csergey8 <c.sergey@protonmail.com>
2017-11-06 10:56:43 +0200

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 import moment from 'moment';
 
 const config = {
-    apiKey: "AIzaSyAf8pybxykJQKpH0VUOKdtUqSvLZQu4P0E",
-    authDomain: "expesify.firebaseapp.com",
-    databaseURL: "https://expesify.firebaseio.com",
-    projectId: "expesify",
-    storageBucket: "expesify.appspot.com",
-    messagingSenderId: "547466098089"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
   

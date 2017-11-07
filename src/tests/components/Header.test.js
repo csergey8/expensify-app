1ccd887a171c13c811176fd9a8b0cbab459dbd99
csergey8 <c.sergey@protonmail.com>
2017-11-07 14:27:41 +0200

    login and logout tests

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,22 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('should render correctly', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => {}} />);
 
     expect(wrapper).toMatchSnapshot();
-    
-    
-    
-    
-    //expect(wrapper.find('h1').length).toBe(1);
-
-
-    //const renderer = new ReactShallowRenderer();
-    //renderer.render(<Header />);
-    //expect(renderer.getRenderOutput()).toMatchSnapshot();
-    //console.log(renderer.getRenderOutput());
 });
 
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled(); 
+});

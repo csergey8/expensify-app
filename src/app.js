75ecd12ef9190c97b42d82552b031e00e1a6ce5a
csergey8 <c.sergey@protonmail.com>
2017-11-07 13:15:09 +0200

    setup simple auth firebase

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import './firebase/firebase';
+import { firebase, googleAuthProvider } from  './firebase/firebase';
 
 const store = configureStore();
 
@@ -31,6 +31,14 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 store.dispatch(startSetExpenses()).then(() => {
     ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+    if(user) {
+        console.log('login');
+    } else {
+        console.log('logout');
+    }
 });
 
 
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import LoginPage from '../../components/LoginPage';
+import { LoginPage } from '../../components/LoginPage';
 
 test('should render correctly LoginPage', () => {
     const wrapper = shallow(<LoginPage />);

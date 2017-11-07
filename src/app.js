226c9a95d76dcd149b23282217ec9c3d16f44972
csergey8 <c.sergey@protonmail.com>
2017-11-07 17:44:23 +0200

    add login logout reducers and tests them

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css'
 import 'normalize.css/normalize.css';
@@ -45,6 +45,8 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if(user) {
+        store.dispatch(login(user.uid));
+        console.log(user.uid); 
         store.dispatch(startSetExpenses()).then(() => {
             renderApp();
             if (history.location.pathname === '/') {
@@ -53,6 +55,7 @@ firebase.auth().onAuthStateChanged((user) => {
         });
         
     } else {
+        store.dispatch(logout());
         renderApp();
         history.push('/');
     }
